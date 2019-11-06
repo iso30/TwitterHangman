550f8198fa3060d74f387bbdef500048352958f2
Mohammed Azad <varsity.azad@gmail.com>
2019-11-05 21:13:47 -0800

    basic game activity design in place

--- app/src/main/java/com/example/twitterhangman/GameActivity.java
+++ app/src/main/java/com/example/twitterhangman/GameActivity.java
@@ -22,15 +22,7 @@ public class GameActivity extends AppCompatActivity {
         Log.d("LOG","in create");
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-/*
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+
     }
 
 }
