79d35242a23ef34a36650ae1601281b201e138a7
Mohammed Azad <varsity.azad@gmail.com>
2019-11-20 21:06:16 -0800

    drag and drop added

--- app/src/main/java/com/example/twitterhangman/GameActivity.java
+++ app/src/main/java/com/example/twitterhangman/GameActivity.java
@@ -1,13 +1,24 @@
 package com.example.twitterhangman;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,9 +30,13 @@ import java.lang.reflect.Array;
 public class GameActivity extends AppCompatActivity {
 
     private ConstraintSet gameConstraintSet;
-    private ImageView[] aUnderscoreView;            ///< Hold the dynamically created underscore views
     private ConstraintLayout gameConstraintLayout;
-
+    private ImageView[] aUnderscoreView;            ///< Hold the dynamically created underscore views
+    private ImageView[] aAlphaView;                 ///< Holds the 28 alphabet Imageviews;
+    private enum Alphabet
+    {
+        A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, Ex, Qu
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +45,9 @@ public class GameActivity extends AppCompatActivity {
         Log.d("LOG","in create");
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        aAlphaView = new ImageView[28];
+        setAlphaArray();
 
         // TODO - hardcoded need to pass in depending on word length
         int NUM_UNDERSCORES = 5;
@@ -64,6 +82,10 @@ public class GameActivity extends AppCompatActivity {
         }
         gameConstraintSet.applyTo(gameConstraintLayout);
 
+        for (ImageView underScoreView : aUnderscoreView)
+        {
+            underScoreView.setOnDragListener(new MyDragListener());
+        }
     }
 
     private void setUnderscoreParams(int underscoreInd, int width, int height)
@@ -73,9 +95,94 @@ public class GameActivity extends AppCompatActivity {
         aUnderscoreView[underscoreInd].setAdjustViewBounds(true);
         aUnderscoreView[underscoreInd].setScaleType(ImageView.ScaleType.FIT_XY);
     }
-    protected void onStart()
+
+    private void setAlphaArray()
     {
-        super.onStart();
+        aAlphaView[0] = findViewById(R.id.alphaA);
+        aAlphaView[1] = findViewById(R.id.alphaB);
+        aAlphaView[2] = findViewById(R.id.alphaC);
+        aAlphaView[3] = findViewById(R.id.alphaD);
+        aAlphaView[4] = findViewById(R.id.alphaE);
+        aAlphaView[5] = findViewById(R.id.alphaF);
+        aAlphaView[6] = findViewById(R.id.alphaG);
+        aAlphaView[7] = findViewById(R.id.alphaH);
+        aAlphaView[8] = findViewById(R.id.alphaI);
+        aAlphaView[9] = findViewById(R.id.alphaJ);
+        aAlphaView[10] = findViewById(R.id.alphaK);
+        aAlphaView[11] = findViewById(R.id.alphaL);
+        aAlphaView[12] = findViewById(R.id.alphaM);
+        aAlphaView[13] = findViewById(R.id.alphaN);
+        aAlphaView[14] = findViewById(R.id.alphaO);
+        aAlphaView[15] = findViewById(R.id.alphaP);
+        aAlphaView[16] = findViewById(R.id.alphaQ);
+        aAlphaView[17] = findViewById(R.id.alphaR);
+        aAlphaView[18] = findViewById(R.id.alphaS);
+        aAlphaView[19] = findViewById(R.id.alphaT);
+        aAlphaView[20] = findViewById(R.id.alphaU);
+        aAlphaView[21] = findViewById(R.id.alphaV);
+        aAlphaView[22] = findViewById(R.id.alphaW);
+        aAlphaView[23] = findViewById(R.id.alphaX);
+        aAlphaView[24] = findViewById(R.id.alphaY);
+        aAlphaView[25] = findViewById(R.id.alphaZ);
+        aAlphaView[26] = findViewById(R.id.alphaExclamation);
+        aAlphaView[27] = findViewById(R.id.alphaQuestion);
+
+        for (int i = Alphabet.A.ordinal(); i < Alphabet.Qu.ordinal(); ++i)
+        {
+            aAlphaView[i].setOnTouchListener(new MyTouchListener());
+        }
+    }
+
+    // Drag class
+    private class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                // TODO - Makes shadow builder really tiny. This shouldnt be a problem since the image will be replaced in the future instead.
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+                        view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                //iew.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Drop class - listens to drag events
+    private class MyDragListener implements View.OnDragListener {
+        // Draws animations while dragging?
+        //Drawable enterShape = getResources().getDrawable(
+        //R.drawable.shape_droptarget);
+        //Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //v.setBackgroundDrawable(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //v.setBackgroundDrawable(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    Drawable img = ((ImageView) view).getDrawable();
+                    ((ImageView) v).setImageDrawable(img);
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //v.setBackgroundDrawable(normalShape);
+                default:
+                    break;
+            }
+            return true;
+        }
 
     }
 
